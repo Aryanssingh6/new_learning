8a73c9ef60b9300cf50e21a8d1608a8a890c15b6
Aryan <singharyan180327@gmail.com>
2025-12-03 21:35:11 +0530

    Maxinarray.java

---
+++ Maxinarray.java
@@ -0,0 +1,15 @@
+public class Maxinarray {
+    public static void main(String[] args) {
+        int[][] arr = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
