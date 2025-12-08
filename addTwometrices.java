83aaa0355cc3e261b1570852e69c2c805a38c626
Aryan <singharyan180327@gmail.com>
2025-12-08 22:40:41 +0530

    addtwometrics.java

--- addTwometrices.java
+++ addTwometrices.java
@@ -1,7 +1,7 @@
 public class addTwometrices {
     public static void main(String[] args) {
         int[] [] a = { {1, 3, 3}, {4, 5, 6}, {7, 8, 9} };
-        int[] [] b = { {9, 5, 7}, {6, 5, 3}, {3, 2, 11} };
+        int[] [] b = { {9, 5, 68}, {6, 5, 43}, {3, 2, 56} };
         int m = a.length;
         int n = a[0].length;
         int res[] [] = new int[m][n];
