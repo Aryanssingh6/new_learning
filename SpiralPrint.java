a5728e6e5221c6fa0a9d4aba409778ec71262513
Aryan <singharyan180327@gmail.com>
2025-12-09 22:33:36 +0530

    SpiralPrint.java

--- SpiralPrint.java
+++ SpiralPrint.java
@@ -1,6 +1,6 @@
 public class SpiralPrint {
     public static void main(String[] args) {
-        int[][] arr = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+        int[][] arr = { {10, 20, 30}, {40, 50, 60}, {70, 80, 90} };
         int m = arr.length;       // number of rows
         int n = arr[0].length;    // number of columns
         int minr = 0, maxr = m - 1;
--- conditionals/sortZerosOnesTwos.java
+++ conditionals/sortZerosOnesTwos.java
@@ -1,6 +1,6 @@
 public class SortZerosOnesTwos {
     public static void main(String[] args) {
-        int[] arr = {0,1,2,0,1,2,1,0,2,0};
+        int[] arr = {0,1,2,0,1,2,1,1,2,0,0,0,2,0};
         int n = arr.length;
         int low = 0, mid = 0, high = n - 1;
         while(mid <= high){
