c815b55896a8b9b32612b664cf88e66e6beaf125
Aryan <singharyan180327@gmail.com>
2025-12-10 23:49:05 +0530

    basicsofarray.java

--- conditionals/basicscodearray.java
+++ conditionals/basicscodearray.java
@@ -7,8 +7,8 @@ public class basicscodearray {
         arr[1] = 20;
         arr[2] = 30;
         arr[3] = 40;
-        arr[4] = 50;
+        arr[4] = 90;
     
-    System.out.println(arr[0]);
+    System.out.println(arr[4]);
 }
 }
